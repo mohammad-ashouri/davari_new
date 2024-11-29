f2c5c8cdd780a291f4d1db5b425a08aaf50f0387
attaxmoon <m.ashouri.wdev@gmail.com>
2024-11-29 21:43:40 +0330

    Add new roles

--- database/seeders/DefaultUsers.php
+++ database/seeders/DefaultUsers.php
@@ -20,9 +20,11 @@ class DefaultUsers extends Seeder
 
         DB::table('users')->insert([
             ['id' => 1, 'username' => 'admin', 'password' => $password, 'name' => 'Super', 'family' => 'Admin', 'type' => 1, 'subject' => 'ادمین کل', 'active' => 1, 'ntcp' => 0, 'adder' => 1],
-            ['id' => 2, 'username' => 'farajnejad', 'password' => $password, 'name' => 'حسن', 'family' => 'فرج نژاد', 'type' => 1, 'subject' => 'معاون', 'active' => 1, 'ntcp' => 0, 'adder' => 1],
-            ['id' => 3, 'username' => 'mostafavizadeh', 'password' => $password, 'name' => 'حسین', 'family' => 'مصطفوی زاده', 'type' => 1, 'subject' => 'مدیر پژوهش', 'active' => 1, 'ntcp' => 0, 'adder' => 1],
-            ['id' => 4, 'username' => 'helali', 'password' => $password, 'name' => 'ابوالفضل', 'family' => 'هلالی', 'type' => 1, 'subject' => 'مدیر اجرایی', 'active' => 1, 'ntcp' => 0, 'adder' => 1],
+            ['id' => 2, 'username' => 'farajnejad', 'password' => $password, 'name' => 'حسن', 'family' => 'فرج نژاد', 'type' => 2, 'subject' => 'معاون', 'active' => 1, 'ntcp' => 0, 'adder' => 1],
+            ['id' => 3, 'username' => 'mostafavizadeh', 'password' => $password, 'name' => 'حسین', 'family' => 'مصطفوی زاده', 'type' => 3, 'subject' => 'مدیر پژوهش', 'active' => 1, 'ntcp' => 0, 'adder' => 1],
+            ['id' => 4, 'username' => 'helali', 'password' => $password, 'name' => 'ابوالفضل', 'family' => 'هلالی', 'type' => 4, 'subject' => 'مدیر نشر داخلی', 'active' => 1, 'ntcp' => 0, 'adder' => 1],
+            ['id' => 5, 'username' => 'modirgp', 'password' => $password, 'name' => 'مدیر', 'family' => 'گروه علمی', 'type' => 5, 'subject' => 'مدیر گروه علمی', 'active' => 1, 'ntcp' => 0, 'adder' => 1],
+            ['id' => 6, 'username' => 'ozvgp', 'password' => $password, 'name' => 'عضو', 'family' => 'گروه', 'type' => 6, 'subject' => 'عضو گروه', 'active' => 1, 'ntcp' => 0, 'adder' => 1],
         ]);
 
         $createRole = Role::create(['name' => 'ادمین کل']);
@@ -111,7 +113,15 @@ class DefaultUsers extends Seeder
         ]);
 
 
-        $createRole = Role::create(['name' => 'مدیر اجرایی']);
+        $createRole = Role::create(['name' => 'مدیر نشر داخلی']);
+        $createRole->givePermissionTo([
+        ]);
+
+        $createRole = Role::create(['name' => 'مدیر گروه علمی']);
+        $createRole->givePermissionTo([
+        ]);
+
+        $createRole = Role::create(['name' => 'عضو گروه']);
         $createRole->givePermissionTo([
         ]);
 
@@ -139,8 +149,16 @@ class DefaultUsers extends Seeder
         $user = User::find(2);
         $user->assignRole([$role->id]);
 
-        $role = Role::where('name', 'مدیر اجرایی')->first();
+        $role = Role::where('name', 'مدیر نشر داخلی')->first();
         $user = User::find(4);
+        $user->assignRole([$role->id]);
+
+        $role = Role::where('name', 'مدیر گروه علمی')->first();
+        $user = User::find(5);
+        $user->assignRole([$role->id]);
+
+        $role = Role::where('name', 'عضو گروه')->first();
+        $user = User::find(6);
         $user->assignRole([$role->id]);
 
 //        $role = Role::where('name', 'ویراستار')->first();
