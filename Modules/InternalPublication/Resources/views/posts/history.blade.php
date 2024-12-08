2339fbecf6d979bd40a0f9e605a27ad0e65964b2
attaxmoon <m.ashouri.wdev@gmail.com>
2024-12-08 23:18:54 +0330

    Internal publication => Fix history view (condition)

--- Modules/InternalPublication/Resources/views/posts/history.blade.php
+++ Modules/InternalPublication/Resources/views/posts/history.blade.php
@@ -8,25 +8,20 @@
             @include('layouts.components.errors')
             @include('layouts.components.success')
             <div class="bg-white rounded shadow p-6 flex flex-col ">
-                @if(empty($movements) or $movements->isEmpty())
-                    <div role="alert" class="alert alert-info">
-                        <i style="font-size: 20px" class="las la-info-circle"></i>
-                        <span>اطلاعاتی یافت نشد!</span>
-                    </div>
-                @else
-                    <table class="datatable w-full border-collapse rounded-lg overflow-hidden text-center datasheet">
-                        <thead>
-                        <tr class="bg-gradient-to-r from-blue-400 to-purple-500 items-center text-center text-white">
-                            <th class="px-6 py-3  font-bold ">کد</th>
-                            <th class="px-6 py-3  font-bold ">نوع</th>
-                            <th class="px-6 py-3  font-bold ">عنوان</th>
-                            <th class="px-6 py-3  font-bold ">توضیحات</th>
-                            <th class="px-6 py-3  font-bold ">فایل پیوست</th>
-                            <th class="px-6 py-3  font-bold ">کاربر ثبت کننده</th>
-                            <th class="px-6 py-3 text-center  font-bold ">تاریخ ثبت</th>
-                        </tr>
-                        </thead>
-                        <tbody class="divide-y divide-gray-300">
+                <table class="datatable w-full border-collapse rounded-lg overflow-hidden text-center datasheet">
+                    <thead>
+                    <tr class="bg-gradient-to-r from-blue-400 to-purple-500 items-center text-center text-white">
+                        <th class="px-6 py-3  font-bold ">کد</th>
+                        <th class="px-6 py-3  font-bold ">نوع</th>
+                        <th class="px-6 py-3  font-bold ">عنوان</th>
+                        <th class="px-6 py-3  font-bold ">توضیحات</th>
+                        <th class="px-6 py-3  font-bold ">فایل پیوست</th>
+                        <th class="px-6 py-3  font-bold ">کاربر ثبت کننده</th>
+                        <th class="px-6 py-3 text-center  font-bold ">تاریخ ثبت</th>
+                    </tr>
+                    </thead>
+                    <tbody class="divide-y divide-gray-300">
+                    @if($movements)
                         @foreach ($movements as $item)
                             <tr class="bg-white">
                                 <td class="px-6 py-4">{{ $item->id }}</td>
@@ -62,36 +57,36 @@
                                 </td>
                             </tr>
                         @endforeach
-                        <tr class="bg-white">
-                            <td class="px-6 py-4">{{ $post->id }}</td>
-                            <td class="px-6 py-4">
-                                اثر در سامانه ثبت شده است
-                            </td>
-                            <td class="px-6 py-4">
-                                -
-                            </td>
-                            <td class="px-6 py-4">
-                                -
-                            </td>
-                            <td class="px-6 py-4">
-                                <a href="{{ Storage::url($post->getInitFile->src) }}">
-                                    <button type="button"
-                                            class="px-4 py-2 mr-3 bg-gray-500 text-white rounded-md hover:bg-gray-600 focus:outline-none focus:ring focus:border-gray-300">
-                                        دانلود فایل اصلی
-                                    </button>
-                                </a>
-                                <hr>
-                            </td>
-                            <td class="px-6 py-4">
-                                {{ $post->adderInfo->name }} {{ $post->adderInfo->family }}
-                            </td>
-                            <td class="px-6 py-4">
-                                {{ Jalalian::fromDateTime($post->created_at)->format('Y/m/d H:i:s') }}
-                            </td>
-                        </tr>
-                        </tbody>
-                    </table>
-                @endif
+                    @endif
+                    <tr class="bg-white">
+                        <td class="px-6 py-4">{{ $post->id }}</td>
+                        <td class="px-6 py-4">
+                            اثر در سامانه ثبت شده است
+                        </td>
+                        <td class="px-6 py-4">
+                            -
+                        </td>
+                        <td class="px-6 py-4">
+                            -
+                        </td>
+                        <td class="px-6 py-4">
+                            <a href="{{ Storage::url($post->getInitFile->src) }}">
+                                <button type="button"
+                                        class="px-4 py-2 mr-3 bg-gray-500 text-white rounded-md hover:bg-gray-600 focus:outline-none focus:ring focus:border-gray-300">
+                                    دانلود فایل اصلی
+                                </button>
+                            </a>
+                            <hr>
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ $post->adderInfo->name }} {{ $post->adderInfo->family }}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ Jalalian::fromDateTime($post->created_at)->format('Y/m/d H:i:s') }}
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
             </div>
 
         </div>
